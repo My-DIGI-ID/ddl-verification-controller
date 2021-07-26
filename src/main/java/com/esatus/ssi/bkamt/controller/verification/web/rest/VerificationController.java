205bfc92d9030dd9aad3ba184674d2769c63d143
Johannes Henrich <J.Henrich@esatus.com>
2021-07-26 09:58:49 +0200

    Removed unused usings

--- src/main/java/com/esatus/ssi/bkamt/controller/verification/web/rest/VerificationController.java
+++ src/main/java/com/esatus/ssi/bkamt/controller/verification/web/rest/VerificationController.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,17 +33,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.esatus.ssi.bkamt.controller.verification.security.AuthoritiesConstants;
 import com.esatus.ssi.bkamt.controller.verification.service.VerificationService;
-import com.esatus.ssi.bkamt.controller.verification.service.dto.UserCreationDTO;
-import com.esatus.ssi.bkamt.controller.verification.service.dto.UserDTO;
 import com.esatus.ssi.bkamt.controller.verification.service.dto.VerificationCreationDTO;
 import com.esatus.ssi.bkamt.controller.verification.service.dto.VerificationDTO;
-import com.esatus.ssi.bkamt.controller.verification.service.exceptions.HotelNotFoundException;
-import com.esatus.ssi.bkamt.controller.verification.service.exceptions.UserAlreadyExistsException;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
